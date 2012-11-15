2a4663e9930c4dfe7d0386b9979a6487c2c08ac3
Sergey Belov <limitium@gmail.com>
2012-11-15 15:01:23 +0400

    cleanup

--- server.php
+++ server.php
@@ -3,10 +3,13 @@ require_once "Ventilator.php";
 
 $server = new Ventilator(true);
 $server->bind("tcp://*:5555");
+
 $server->setGenerator(function () {
     return mt_rand(1, 1000);
 });
+
 $server->setResponder(function ($data) {
     print_r("Got data:$data" . PHP_EOL);
 });
+
 $server->listen();
