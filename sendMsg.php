68077f7a98a7b96805206054c1defa9961bacd9c
sergey belov <limitium@gmail.com>
2012-11-15 22:49:12 +0400

    spam fix

--- sendMsg.php
+++ sendMsg.php
@@ -1,48 +1,60 @@
 <?php
+error_reporting(E_ALL);
+date_default_timezone_set("UTC");
 
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . "Util" . DIRECTORY_SEPARATOR . "Curler.php";
 
-require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . "CnCApi.php";
-require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . "GameObjects.php";
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . "CCAuth" . DIRECTORY_SEPARATOR . "CCAuth.php";
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . "CCApi" . DIRECTORY_SEPARATOR . "CCApi.php";
+
+$servers = require dirname(__FILE__) . DIRECTORY_SEPARATOR . "servers.php";
+
 
 $start = microtime(true);
 
+$server = $servers[$argv[1]];
 
-$serverName = $argv[1];
-$api = new CCApi($serverName);
+$authorizator = new CCAuth("limitium@gmail.com", "qweqwe123");
+$ses = $authorizator->getSession();
 
-$api->openSession();
+$api = new CCApi($server["Url"], $ses);
 
-$title = "World minimap!!!";
-$msg = "World minimap!
+if ($api->openSession()) {
+
+    $title = "World minimap!!!";
+    $msg = "World minimap!
 Everyone welcome!
 [url]http://map.tiberium-alliances.com/[/url]
 
 
 ";
 
-$post = "
- Added Мир 1, Världen 1, Svet 1, Lume 1, Verden 1, Világ 1, Maailma 1, Verden 1, Svět 1.";
-$data = $api->poll(array(
-    "requests" => "WC:A\fCTIME:$time\fCHAT:\fWORLD:\fGIFT:\fACS:0\fASS:0\fCAT:0\f"
-));
-foreach ($data as $part) {
-    if ($part->t == "FORUM") {
-        $forumId = $part->d->f[1]->i;
-        break;
-    }
-}
-if (!$forumId) {
-    file_put_contents("c:/WebServers/home/cc/www/parser/spam/errorForum".$serverName, json_encode($data));
-    print_r("No forumId found");
-}
-$fileName = dirname(__FILE__) . DIRECTORY_SEPARATOR . "spam" . DIRECTORY_SEPARATOR . $serverName;
-if (!is_file($fileName) || !($threadId = file_get_contents($fileName))) {
-    $id = $api->createThread($title, $msg.$post, $forumId);
-    file_put_contents($fileName, $id);
-} else {
-    $api->addPost($post, $forumId, $threadId);
-    file_put_contents(dirname(__FILE__) . DIRECTORY_SEPARATOR . "spam" . DIRECTORY_SEPARATOR . "p" . $serverName, 1);
-}
-$api->close();
+    $post = "
+ Added World 51, World 52, World 53, Мир 8, Welt 19";
 
-print_r("Time: " . (microtime(true) - $start));
+    $time = CCApi::getTime();
+    $data = $api->poll(array(
+        "requests" => "WC:A\fCTIME:$time\fCHAT:\fWORLD:\fGIFT:\fACS:0\fASS:0\fCAT:0\f"
+    ));
+    foreach ($data as $part) {
+        if ($part->t == "FORUM") {
+            $forumId = $part->d->f[1]->i;
+            break;
+        }
+    }
+    if (!$forumId) {
+        file_put_contents("c:/WebServers/home/cc/www/parser/spam/errorForum" . $server["Id"], json_encode($data));
+        print_r("No forumId found");
+    }
+    $fileName = dirname(__FILE__) . DIRECTORY_SEPARATOR . "spam" . DIRECTORY_SEPARATOR . $server["Id"];
+    if (!is_file($fileName) || !($threadId = file_get_contents($fileName))) {
+        $id = $api->createThread($title, $msg . $post, $forumId);
+        file_put_contents($fileName, $id);
+    } else {
+        $api->addPost($post, $forumId, $threadId);
+        file_put_contents(dirname(__FILE__) . DIRECTORY_SEPARATOR . "spam" . DIRECTORY_SEPARATOR . "p" . $server["Id"], 1);
+    }
+    $api->close();
+
+    print_r("Time: " . (microtime(true) - $start));
+}
