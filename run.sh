7391a156325f7801d6e214bad2e4ee8927c4e82b
limitium <limitium@gmail.com>
2015-09-02 02:45:34 +0300

    runner change

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@ killall -9 php
 nohup php src/Sparser.php > /var/log/taparser 2>&1&
 nohup php src/Wparser.php > /dev/null 2>&1&
 nohup php src/Wparser.php > /dev/null 2>&1&
+nohup php src/Wparser.php > /dev/null 2>&1&
+nohup php src/Wparser.php > /dev/null 2>&1&
 
 nohup php src/Suploader.php > /dev/null 2>&1&
 nohup php src/Wuploader.php > /dev/null 2>&1&
